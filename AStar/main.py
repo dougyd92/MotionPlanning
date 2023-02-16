3fd45d38cfcbd15a2605f9c2a274dd971183c742
Douglas de Jesus <douglasdejesus92@gmail.com>
2023-02-16 02:57:11 -0500

    Animate reconstructed path

--- AStar/main.py
+++ AStar/main.py
@@ -1,12 +1,13 @@
 # import ast
 from collections import deque
+from configparser import ConfigParser
 from enum import IntEnum
 
-from configparser import ConfigParser
 from matplotlib.animation import FuncAnimation  # type: ignore
 from matplotlib.colors import ListedColormap  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
+from typing_extensions import TypeAlias
 
 
 class NodeState(IntEnum):
@@ -17,6 +18,9 @@ class NodeState(IntEnum):
     GOAL = 4
 
 
+Coordinate: TypeAlias = tuple[int, int]
+
+
 class SingleAgentAStar:
     """
     Not actually A-Star yet.
@@ -24,18 +28,18 @@ class SingleAgentAStar:
     """
 
     def __init__(
-        self, height: int, width: int, start: tuple[int, int], goal: tuple[int, int]
+        self, height: int, width: int, start: Coordinate, goal: Coordinate
     ) -> None:
         self.height = height
         self.width = width
         self.start = start
         self.goal = goal
-        self.frontier: deque[tuple[int, int]] = deque()
-        self.reached: set[tuple[int, int]] = set()
+        self.frontier: deque[Coordinate] = deque()
+        self.reached_from: dict[Coordinate, Coordinate] = {}
         self.history: list[np.ndarray] = []
         self.update_history()
 
-    def get_neighbors(self, coordinate: tuple[int, int]) -> list[tuple[int, int]]:
+    def get_neighbors(self, coordinate: Coordinate) -> list[Coordinate]:
         (x, y) = coordinate
         neighbors = []
         if x > 0:
@@ -51,7 +55,7 @@ class SingleAgentAStar:
     def update_history(self) -> None:
         grid = np.zeros((self.height, self.width))
         grid[self.goal[1]][self.goal[0]] = NodeState.GOAL
-        for x, y in self.reached:
+        for x, y in self.reached_from:
             grid[y][x] = NodeState.REACHED
         for x, y in self.frontier:
             grid[y][x] = NodeState.FRONTIER
@@ -60,25 +64,54 @@ class SingleAgentAStar:
 
     def find_best_path(self) -> None:
         self.frontier.append(self.start)
-        self.reached.add(self.start)
+        self.reached_from[self.start] = self.start
 
         while len(self.frontier) > 0:
             current_node = self.frontier.popleft()
             for next_node in self.get_neighbors(current_node):
-                if next_node not in self.reached:
+                if next_node not in self.reached_from:
                     self.frontier.append(next_node)
-                    self.reached.add(next_node)
+                    self.reached_from[next_node] = current_node
                     self.update_history()
         self.update_history()
 
+    def reconstruct_path(self) -> list[Coordinate]:
+        current_node = self.goal
+        path = []
+        while current_node != self.start:
+            path.append(current_node)
+            current_node = self.reached_from[current_node]
+        path.append(self.start)
+        path.reverse()
+        return path
 
-def animation_step(i, im, history: list[np.ndarray]):
-    im.set_array(history[i])
+
+def animation_step(i, im, history: list[np.ndarray], path: list[Coordinate]):
+    if i < len(history):
+        im.set_array(history[i])
+    else:
+        j = i - len(history)
+        (from_x, from_y) = path[j]
+        (to_x, to_y) = path[j + 1]
+        im.axes.arrow(
+            from_x,
+            from_y,
+            (to_x - from_x) * 0.8,
+            (to_y - from_y) * 0.8,
+            head_width=0.25,
+            length_includes_head=True,
+            zorder=2,
+        )
     return [im]
 
 
 def animate_search(
-    grid_height: int, grid_width: int, history: list[np.ndarray]
+    grid_height: int,
+    grid_width: int,
+    start: Coordinate,
+    goal: Coordinate,
+    history: list[np.ndarray],
+    path: list[Coordinate],
 ) -> None:
     fig, ax = plt.subplots()
     ax.grid(which="major", axis="both", linestyle="-", color="k", linewidth=2)
@@ -94,14 +127,17 @@ def animate_search(
 
     cmap = ListedColormap(["white", "red", "blue", "grey", "green"])
     im = ax.imshow(history[0], cmap=cmap)
+    ax.annotate("START", start, ha="center")
+    ax.annotate("GOAL", goal, ha="center")
     _anim = FuncAnimation(
         fig,
         animation_step,
-        fargs=(im, history),
-        frames=len(history),
-        interval=250,
-        repeat_delay=3000,
+        fargs=(im, history, path),
+        frames=len(history) + len(path) - 1,
+        interval=2,
+        repeat=False,
     )
+
     plt.show()
 
 
@@ -121,7 +157,16 @@ def main():
         grid_height, grid_width, (robot_x, robot_y), (goal_x, goal_y)
     )
     search.find_best_path()
-    animate_search(grid_height, grid_width, search.history)
+    path = search.reconstruct_path()
+
+    animate_search(
+        grid_height,
+        grid_width,
+        (robot_x, robot_y),
+        (goal_x, goal_y),
+        search.history,
+        path,
+    )
 
 
 if __name__ == "__main__":
